cb6984822aaa679c44e292ead41596f2c759058a
Marvin Schopf <marvin@schopf.biz>
2021-03-04 00:06:01 +0100

    Add styles

--- games/snake/src/App.tsx
+++ games/snake/src/App.tsx
@@ -4,10 +4,6 @@ import SnakeGame from "./SnakeGame";
 
 export default class App extends Component {
 	render() {
-		return (
-			<div style={{ width: "100%", height: "100%", margin: 0 }}>
-				<SnakeGame />
-			</div>
-		);
+		return <SnakeGame />;
 	}
 }
